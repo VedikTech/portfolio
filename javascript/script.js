da6139205cf3fbb9be6a230660bff008e01f9a31
vedikathote <vedikathote@gmail.com>
2024-05-02 18:56:51 +0530

    add some changes

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+//bars
+
+console.log("hey vedika");
+
+function barfun()
+{
+
+   document.body.style.backgroundColor="green";
+    classList.add("barsnavbar").innerHTML="opacity:1";
+}
+
+let bars=document.getElementsByClassName(".bars");
+bars.addEventListener("toggle",barfun);
+
