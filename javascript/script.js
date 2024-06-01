b80fe71d8c0fc211a72b2b15519bee593a8836a8
vedikathote <vedikathote@gmail.com>
2024-06-02 00:43:56 +0530

    made some changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,42 @@
-//bars
 
-console.log("hey vedika");
 
-function barfun()
-{
+    let toggle_btn = document.getElementById("toggle");
+    toggle_btn.addEventListener("click", () => {
+        console.log("hey vedu");
+        toggle_btn.classList.toggle("navitem2");
+    });
 
-   document.body.style.backgroundColor="green";
-    classList.add("barsnavbar").innerHTML="opacity:1";
-}
 
-let bars=document.getElementsByClassName(".bars");
-bars.addEventListener("toggle",barfun);
+     
+        
+     
+   
 
+
+/*toggle night mode light mode
+let btn=document.getElementById("btnmode");
+let body=document.querySelector("body");
+let mode="dark";
+btn.addEventListener("click", ()=>{
+if(mode==="dark")
+    {
+
+        mode="light";
+        body.classList.add("bodylightmode");
+        body.classList.remove("body");
+     console.log("light")
+    }
+    else{
+
+        mode="dark";
+        body.classList.add("body");
+        body.classList.remove("bodylightmode");
+        console.log("dark");
+    }
+ 
+
+  
+   
+
+});
+*/
