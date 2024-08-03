3a00b94a4301308862173ce3d83027e33d33c053
vedikathote <vedikathote@gmail.com>
2024-08-03 11:44:28 +0530

    add some changes

--- javascript/script.js
+++ javascript/script.js
@@ -5,14 +5,14 @@ let navitem = document.querySelector(".navitem");
 let navLinks = navitem.querySelectorAll("a");
 
 // Event listener for navicon click (shows nav items)
-navicon.addEventListener("click", function() {
+navicon.addEventListener("click", function () {
   navitem.classList.toggle("active");
   cross.style.display = "block";
   navicon.style.display = "none";
 });
 
 // Event listener for cross click (hides nav items)
-cross.addEventListener("click", function() {
+cross.addEventListener("click", function () {
   navitem.classList.toggle("active");
   cross.style.display = "none";
   navicon.style.display = "block";
@@ -20,12 +20,13 @@ cross.addEventListener("click", function() {
 
 // Event listeners for each nav link click (hides nav items after clicking a link)
 for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", function() {
-    if (window.innerWidth <= 991) { // Check if screen width is less than or equal to 991px
+  navLinks[i].addEventListener("click", function () {
+    if (window.innerWidth <= 991) {
+      // Check if screen width is less than or equal to 991px
       navitem.classList.remove("active");
       cross.style.display = "none";
       navicon.style.display = "block";
-    } 
+    }
   });
 }
 
@@ -34,12 +35,12 @@ let proimg = document.getElementsByClassName("proimg");
 let overscreen = document.getElementsByClassName("overscreen");
 
 for (let i = 0; i < proimg.length; i++) {
-  proimg[i].addEventListener("click", function() {
+  proimg[i].addEventListener("click", function () {
     if (overscreen[i]) {
       overscreen[i].classList.toggle("show");
     }
   });
-  proimg[i].parentElement.addEventListener("mouseleave", function() {
+  proimg[i].parentElement.addEventListener("mouseleave", function () {
     if (overscreen[i]) {
       overscreen[i].classList.remove("show");
     }
